522ef62014920229c328b7db5db9b626a0b3a0b7
gabriel-severo <gabriel.ivi@hotmail.com>
2020-04-03 16:40:46 -0300

    Adding handleDecifrar

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Inicio.css'
+import api from '../services/api'
 export default function Inicio(){
+    const [message, setMessage] = useState('')
+    const [key, setKey] = useState('')
+    const [result, setResult] = useState('')
+    async function handleDecifrar(){
+        const response = await api.post('/decifrar', {
+            message: message,
+            key: key
+        })
+        setResult(response.data.response)
+    }
+    function handleCifrar(){
+        console.log('Cifrar')
+    }
     return (
         <div className="container">
-            <form>
-                <textarea type="text" rows="10" cols="30" name="texto"/>
+            <form onSubmit={e => {e.preventDefault()}}>
+                <textarea type="text" rows="10" cols="30" name="texto" value={message} onChange={e => {setMessage(e.target.value)}}/>
                 <div className="acoes">
-                    <input type="submit" value="Cifrar"/>
-                    <input type="number" name="key" placeholder="KEY" autocomplete="off"/>
-                    <input type="submit" value="Decifrar"/>
+                    <input type="submit" value="Cifrar" onClick={handleCifrar}/>
+                    <input type="number" name="key" placeholder="KEY" autoComplete="off" value={key} onChange={e => {setKey(e.target.value)}}/>
+                    <input type="submit" value="Decifrar" onClick={handleDecifrar}/>
                 </div>
-                <textarea type="text" rows="10" cols="30" name="resultado"/>
+                <textarea type="text" rows="10" cols="30" name="resultado" defaultValue={result}/>
             </form>
         </div>
     )
