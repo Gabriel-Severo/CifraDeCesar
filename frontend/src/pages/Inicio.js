60c7aaf73f419ef30b8083f3fc2a9caac29c599c
gabriel-severo <gabriel.ivi@hotmail.com>
2020-04-03 16:12:40 -0300

    Adding style to initial page

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import './Inicio.css'
 export default function Inicio(){
     return (
         <div className="container">
-            <input type="text" name="texto"/>
-            <input type="submit" value="Cifrar"/>
-            <input type="text" name="key"/>
-            <input type="submit" value="Decifrar"/>
-            <input type="text" name="resultado"/>
+            <form>
+                <textarea type="text" rows="10" cols="30" name="texto"/>
+                <div className="acoes">
+                    <input type="submit" value="Cifrar"/>
+                    <input type="number" name="key" placeholder="KEY" autocomplete="off"/>
+                    <input type="submit" value="Decifrar"/>
+                </div>
+                <textarea type="text" rows="10" cols="30" name="resultado"/>
+            </form>
         </div>
     )
 }
