69fc4d2875c3bef70148cb5526dda807e3b04672
gabriel-severo <gabriel.ivi@hotmail.com>
2020-04-03 17:05:59 -0300

    Fixed some problems with CifrarController

--- backend/src/controller/CifrarController.js
+++ backend/src/controller/CifrarController.js
@@ -3,7 +3,7 @@ function handleCifrar(msg, key){
     for (letter of msg){
         let letterAscii = letter.charCodeAt(0)
         if (letterAscii >= 97 && letterAscii <= 122){
-            letterAscii += key
+            letterAscii += parseInt(key)
             if (letterAscii > 122){
                 letterAscii = 96 + (122 - letterAscii)
                 cifrado += String.fromCharCode(letterAscii)
@@ -21,6 +21,6 @@ module.exports = {
     async index(req, res){
         const {message, key} = req.body
         const response = handleCifrar(message, key)
-        res.json({'response': response})
+        return res.json({'response':response})
     }
 }
